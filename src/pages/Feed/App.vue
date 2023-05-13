6436fd38b6fa26b2dc4aa99a0db35027fcde4984
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-05-13 23:24:11 +0800

    sidemenu design change when bootstrap css added?

--- src/pages/Feed/App.vue
+++ src/pages/Feed/App.vue
@@ -2,27 +2,33 @@
     <div id="main-container">
         <NavSidebar/>
         <div id="main-content">
-            <div class="col-md-6 pink-header-search"> 
+            <div class="col-md-12 pink-header-search"> 
                 <input class="search-bar" type="text" placeholder="search">
             </div>
             <h1>Following</h1>
             <div class="blog-container">
-                <div class="blog-content col-md-12">
-                    <div class="row">
-                        <span>
-                            <img class="profilepic" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
-                            <p>Joshua Tan
-                            </p>
+                <div class="row">
+                    <div class="col-6">
+                        <span class="row">
+                            <div class="col-2">
+                                <img class="profilepic" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
+                            </div>
+                            <div class="col-10">
+                                <p>Joshua Tan</p>
+                                <p style="margin-top: -20px;">@youdontknowme</p>
+                            </div>
                         </span>
+                    </div>
+                    <div class="col-6">
                         <span>
                             <p>
                                 Posted 6 days ago
                             </p>
                         </span>
                     </div>
-                    <div class="row">
-                        <img class="blog-image" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
-                    </div>
+                </div>
+                <div class="row">
+                    <img class="blog-image" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
                 </div>
             </div>
 
