0f6c2f9555558f40e8d881a56ed7fe2d27aafd2e
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-05-14 15:45:21 +0800

    .

--- src/pages/Feed/App.vue
+++ src/pages/Feed/App.vue
@@ -19,18 +19,18 @@
             <br>
             <div class="blog-container">
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-md-6">
                         <span class="row">
-                            <div class="col-2">
+                            <div class="col-md-2">
                                 <img class="profilepic" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
                             </div>
-                            <div class="col-10">
+                            <div class="col-md-10">
                                 <p>Joshua Tan</p>
                                 <p style="margin-top: -20px;">@youdontknowme</p>
                             </div>
                         </span>
                     </div>
-                    <div class="col-6 d-flex justify-content-end">
+                    <div class="col-md-6 d-flex justify-content-end">
                         <span>
                             <p>
                                 Posted 6 days ago
@@ -48,7 +48,27 @@
                 </div>
                 <div class="row">
                     <p class="blog-caption">
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol                         Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
                         Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol                         Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+
                     </p>
                 </div>
                 <div class="d-flex justify-content-end">
@@ -59,7 +79,57 @@
                 </div>
             </div>
 
+            <div class="blog-container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <span class="row">
+                            <div class="col-md-2">
+                                <img class="profilepic" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
+                            </div>
+                            <div class="col-md-10">
+                                <p>Joshua Tan</p>
+                                <p style="margin-top: -20px;">@youdontknowme</p>
+                            </div>
+                        </span>
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-end">
+                        <span>
+                            <p>
+                                Posted 6 days ago
+                            </p>
+                        </span>
+                    </div>
+                </div>
+                <div class="row">
+                    <img class="blog-image" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
+                </div>
+                <div class="row">
+                    <p>
 
+                    </p>
+                </div>
+                <div class="row">
+                    <p class="blog-caption">
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol                         Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+                        Went to the playground with my baby cousn today!! Here are some photos of him. #lol 
+
+                    </p>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <!-- Retrieve number of comments -->
+                    <div class="p-2">
+                        <button>View 13 comments</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
