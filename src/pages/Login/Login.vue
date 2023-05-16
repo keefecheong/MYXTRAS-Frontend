6b1f04fea8afd098dbb760ebdb0f79cb0ccccc42
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-05-16 23:47:48 +0800

    login & registration

---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="main-container">
+        <NavSidebar/>
+        <div id="main-content">
+            <h1>Forum</h1>
+        </div>
+    </div>
+
+</template>
+  
+<script>
+import NavSidebar from '../../components/NavSidebar.vue';
+
+export default {
+    components: {
+        NavSidebar
+    },
+    data() {
+            return {
+                maximized: false
+            }
+        },
+}
+</script>
