58ec54db3c0d925a117c33d834e968cb1bdaadae
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-05-17 00:07:21 +0800

    created login & regis pages

---
+++ src/pages/SetUpProfile/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './Setupprofile.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+const app = createApp(App);
+
+app.mount('#app');
