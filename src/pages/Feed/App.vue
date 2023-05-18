3d0e9191dc0d0ab4895573af405e9f549c1542ad
Yong Zi Ren <s10219524@connect.np.edu.sg>
2023-05-18 22:27:30 +0800

    fix button and profile pic

--- src/pages/Feed/App.vue
+++ src/pages/Feed/App.vue
@@ -9,8 +9,8 @@
                     <!-- check for identity after authentication -->
                     <p class="username"><br></p>
                     <p class="school"></p>
-                    <img class="headerprofilepic" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
-                    <a href="/login.html"><button type="button" class="btn btn-light">Log in</button></a>
+                    <img v-if="loggedin" class="headerprofilepic" src="https://www.vhv.rs/dpng/d/439-4393951_random-picture-of-a-person-hd-png-download.png">
+                    <a v-else href="/login.html" class="codepen-button"><span>Log in</span></a>
                 </div>
             </div>
             <br>
@@ -247,6 +247,60 @@
   
 .active, .dot1:hover, .dot2:hover {
     background-color: #717171;
+}
+
+/* Login button CSS */
+.codepen-button {
+  display: block;
+  cursor: pointer;
+  color: rgb(0, 0, 0);
+  margin: auto;
+  margin-right: 4rem;
+  position: relative;
+  text-decoration: none;
+  font-weight: 600;
+  border-radius: 10px;
+  overflow: hidden;
+  padding: 3px;
+  isolation: isolate;
+}
+
+.codepen-button::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 400%;
+  height: 100%;
+  background: linear-gradient(115deg,#4fcf70,#fad648,#a767e5,#12bcfe,#44ce7b);
+  background-size: 25% 100%;
+  animation: an-at-keyframe-css-at-rule-that-translates-via-the-transform-property-the-background-by-negative-25-percent-of-its-width-so-that-it-gives-a-nice-border-animation_-We-use-the-translate-property-to-have-a-nice-transition-so-it_s-not-a-jerk-of-a-start-or-stop .75s linear infinite;
+  animation-play-state: paused;
+  translate: -5% 0%;
+  transition: translate 0.25s ease-out;
+}
+
+.codepen-button:hover::before {
+  animation-play-state: running;
+  transition-duration: 0.75s;
+  translate: 0% 0%;
+}
+
+@keyframes an-at-keyframe-css-at-rule-that-translates-via-the-transform-property-the-background-by-negative-25-percent-of-its-width-so-that-it-gives-a-nice-border-animation_-We-use-the-translate-property-to-have-a-nice-transition-so-it_s-not-a-jerk-of-a-start-or-stop {
+  to {
+    transform: translateX(-25%);
+  }
+}
+
+.codepen-button span {
+  position: relative;
+  display: block;
+  padding: 0.7rem 1.5rem;
+  font-size: 1.1rem;
+  background: #ffffff;
+  border-radius: 10px;
+  height: 100%;
 }
 </style>
 
