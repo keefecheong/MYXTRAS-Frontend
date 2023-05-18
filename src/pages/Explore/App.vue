debcbd75b18c6677807e70148fb016be0ed07adf
Yong Zi Ren <s10219524@connect.np.edu.sg>
2023-05-18 22:58:59 +0800

    refactor code and started design on explore

--- src/pages/Explore/App.vue
+++ src/pages/Explore/App.vue
@@ -1,20 +1,26 @@
 <template>
-
     <div id="main-container">
         <NavSidebar/>
         <div id="main-content">
+            <SearchBar/>
             <h1>Explore</h1>
         </div>
     </div>
 
 </template>
+<style>
+@import url('../../styles/main.css');
+
+</style>
 
 <script>
 import NavSidebar from '../../components/NavSidebar.vue';
+import SearchBar from '../../components/SearchBar.vue';
 
 export default {
     components: {
-        NavSidebar
+        NavSidebar,
+        SearchBar
     }
 }
 </script>
