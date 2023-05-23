d1435507a45beab4e2bc245f82667bdfaea72e5d
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2023-05-23 22:55:54 +0800

    Basic implementation of form to create posts Implemented router to toggle between viewing feed and creating new post

---
+++ src/views/FeedBlogView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>feed blog view</h1>
+    
+</template>
+
+<script>
+    import BlogLayout from '../components/BlogLayout.vue';
+
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-require('dotenv').config();
-
-// custom plugin to redirect / requests to /index.html
+// custom plugin to redirect / requests to /feed.html
 const redirectIndexPlugin = {
   name: 'redirect-index',
   configureServer(server) {
     server.middlewares.use((req, res, next) => {
-      if (req.url === '/') {
+      if (req.url === '/' || req.url === '/create') {
         req.url = '/feed.html';
       }
       next();
@@ -19,14 +17,13 @@ const redirectIndexPlugin = {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), redirectIndexPlugin],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [vue(), redirectIndexPlugin],
+    server: {
+      port: env.VITE_PORT
     }
-  },
-  server: {
-    port: process.env.PORT
   }
 })
