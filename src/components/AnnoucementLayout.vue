fab288e8634770ee409362bc3b13e1b0eaa0c289
Yong Zi Ren <s10219524@connect.np.edu.sg>
2023-05-30 01:29:30 +0800

    done announcement

---
+++ src/components/AnnoucementLayout.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="row a-all">
+        <div class="a-container red">
+            <h1 class="a-header">Halloween 👻</h1>
+            <p class="a-caption">
+                As the crisp autumn air settles in and the leaves crunch underfoot, 
+                the time for spooky fun draws near. Halloween is just around the corner, 
+                and with it comes the thrill of the unknown and the excitement of dressing up in ghoulish costumes. 
+                So grab your broomsticks and don your masks, because it's time to embrace the spooky season and all the thrills that come with it!
+                <br>Let’s celebrate Halloween together at Ngee Ann Polytechnic! 🦇
+            </p>
+            <img class="a-image" src="https://i.imgur.com/1qkYzYH.jpg" alt="forum-image">
+        </div>
+        <div class="a-container yellow">
+            <h1 class="a-header">Halloween 👻</h1>
+            <p class="a-caption">
+                As the crisp autumn air settles in and the leaves crunch underfoot, 
+                the time for spooky fun draws near. Halloween is just around the corner, 
+                and with it comes the thrill of the unknown and the excitement of dressing up in ghoulish costumes. 
+                So grab your broomsticks and don your masks, because it's time to embrace the spooky season and all the thrills that come with it!
+                <br>Let’s celebrate Halloween together at Ngee Ann Polytechnic! 🦇
+            </p>
+            <img class="a-image" src="https://i.imgur.com/1qkYzYH.jpg" alt="forum-image">
+        </div>
+        <div class="a-container pink">
+            <h1 class="a-header">Halloween 👻</h1>
+            <p class="a-caption">
+                As the crisp autumn air settles in and the leaves crunch underfoot, 
+                the time for spooky fun draws near. Halloween is just around the corner, 
+                and with it comes the thrill of the unknown and the excitement of dressing up in ghoulish costumes. 
+                So grab your broomsticks and don your masks, because it's time to embrace the spooky season and all the thrills that come with it!
+                <br>Let’s celebrate Halloween together at Ngee Ann Polytechnic! 🦇
+            </p>
+            <img class="a-image" src="https://i.imgur.com/1qkYzYH.jpg" alt="forum-image">
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+
+<style>
+@import url('../styles/main.css');
+
+.a-container {
+    display: inline-block;
+    border: #133B5B;
+    border-style: solid 1rem;
+    margin: 50px 30px 30px 30px;
+    margin-top: 50px;
+    box-shadow: 1px 1px 5px 1px rgba(65, 48, 48, 0.3);;
+    border-radius: 10px;
+    width: 40%;
+    padding-bottom: 20px;
+}
+
+.a-header {
+    font-size: 30px;
+    font-weight: bold;
+    border-bottom: #000000 2px solid;
+    padding: 40px;
+}
+
+.a-caption {
+    font-size: 15px;
+    padding: 30px;
+    text-align: justify;
+    word-wrap: break-word;
+    white-space: pre-wrap;
+}
+
+.a-image {
+    border-radius: 10px;
+    width: 95%;
+    height: 10rem;
+    display: flex;
+    margin: auto;
+}
+
+.a-all {
+    overflow: auto;
+    white-space: nowrap;
+}
+
+.pink {
+    background-color: #FF7BE2;
+}
+
+.blue {
+    background-color: #6FE5FF;
+}
+
+.green {
+    background-color: #6FFFA8;
+}
+
+.yellow {
+    background-color: #FFE27B;
+}
+
+.dark-blue {
+    background-color: #7B88FF;
+}
+
+.orange {
+    background-color: #FFAB6F;
+}
+
+.red {
+    background-color: #FC5454;
+}
+</style>
--- src/pages/Events/Events.vue
+++ src/pages/Events/Events.vue
@@ -1,9 +1,9 @@
 <template>
-
     <div id="main-container">
         <NavSidebar/>
         <div id="main-content">
-            <h1>Events</h1>
+            <h1 class="row pink-header">Xtra EVENTS! 🔊</h1>
+            <AnnoucementLayout/>
         </div>
     </div>
 
@@ -11,10 +11,24 @@
 
 <script>
 import NavSidebar from '../../components/NavSidebar.vue';
+import AnnoucementLayout from '../../components/AnnoucementLayout.vue'; 
 
 export default {
     components: {
-        NavSidebar
+        NavSidebar,
+        AnnoucementLayout
     }
 }
 </script>
+
+<style>
+@import url('../../styles/main.css');
+
+.pink-header {
+    margin: 0 -12px 0 -12px;
+    padding-top: 5vh;
+    padding-bottom: 5vh;
+    background-color: var(--primary);
+}
+
+</style>
