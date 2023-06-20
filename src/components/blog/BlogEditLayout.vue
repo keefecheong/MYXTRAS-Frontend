a9748b96d5142dcd200df998ef9cabd64d13f310
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2023-06-20 23:55:50 +0800

    fix edit blog on feed page overlapped by searchbar

--- src/components/blog/BlogEditLayout.vue
+++ src/components/blog/BlogEditLayout.vue
@@ -62,7 +62,7 @@ export default {
     width: 100%;
     height: 100%;
     background-color: rgb(0, 0, 0, 0.5);
-    z-index: 1;
+    z-index: 10;
     display: flex;
     justify-content: center;
     align-items: center;
