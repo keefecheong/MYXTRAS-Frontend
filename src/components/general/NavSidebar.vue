55cf646142b4fbf0de4964994c60d093e8e1d9b6
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2023-06-27 23:14:08 +0800

    put socket in navbar to make user status online throughout the app

--- src/components/general/NavSidebar.vue
+++ src/components/general/NavSidebar.vue
@@ -139,141 +139,162 @@
 </template>
 
 <script>
-    export default {
-        mounted() {
-            // call handleResize to set up sidebar based on initial window dimensions
-            this.handleResize();
-            
-            // check for the current page to style links
-            this.setCurrent();
+import { socket } from '../../utils/chat/chatSocket.js';
 
-            // set scroll listener to make sidebar stick dynamically
-            window.addEventListener('scroll', this.handleScroll);
+export default {
+    mounted() {
+        // call handleResize to set up sidebar based on initial window dimensions
+        this.handleResize();
+        
+        // check for the current page to style links
+        this.setCurrent();
 
-            // set resize listener to make sidebar responsive
-            window.addEventListener('resize', this.handleResize);
+        // set scroll listener to make sidebar stick dynamically
+        window.addEventListener('scroll', this.handleScroll);
+
+        // set resize listener to make sidebar responsive
+        window.addEventListener('resize', this.handleResize);
+    },
+    updated() {
+        // check for the current page after sidebar updates to style links
+        this.setCurrent();
+
+        // manage expand/collapse navbar
+        this.setNavbar();
+
+        // set margin on main content when sidebar is maximized/minimized
+        this.setMargin();
+    },
+    unmounted() {
+        // remove event listeners on unmount
+        window.removeEventListener('scroll', this.handleScroll);
+        window.removeEventListener('resize', this.handleResize);
+    },
+    data() {
+        return {
+            maximized: false,
+            expanded: false,
+            narrow: false,
+            currentLocation: 0
+        }
+    },
+    methods: {
+        // function to toggle value of "maximized" (toggle normal sidebar)
+        toggleSidebar() {
+            this.maximized = !this.maximized;
         },
-        updated() {
-            // check for the current page after sidebar updates to style links
-            this.setCurrent();
+        // function to toggle value of "expanded" (toggle narrow sidebar)
+        toggleNavbar() {
+            // if the navbar is going to be expanded then hide the main content so only the navbar is shown
+            // otherwise show the main content again
+            // save the user's current scroll location of the page to return the same content later
+            if (!this.expanded) {
+                this.currentLocation = window.scrollY;
+                document.getElementById('main-content').classList.add('hidden');
+            }
+            else {
+                document.getElementById('main-content').classList.remove('hidden');
+            }
 
-            // manage expand/collapse navbar
-            this.setNavbar();
+            this.expanded = !this.expanded;
+        },
+        // handle scroll logic
+        handleScroll() {
+            // don't do anything if sidebar is on narrow screens
+            if (this.narrow) {
+                return;
+            }
 
-            // set margin on main content when sidebar is maximized/minimized
-            this.setMargin();
+            // check if the scroll is upwards or downwards and handle it accordingly
+            if (window.scrollY < this.lastScrollY) {
+                this.scrollUp();
+            }
+            else {
+                this.scrollDown();
+            }
+
+            this.lastScrollY = window.scrollY;
         },
-        unmounted() {
-            // remove event listeners on unmount
-            window.removeEventListener('scroll', this.handleScroll);
-            window.removeEventListener('resize', this.handleResize);
-        },
-        data() {
-            return {
-                maximized: false,
-                expanded: false,
-                narrow: false,
-                currentLocation: 0
+        // handle resize logic
+        handleResize() {
+            // check if window is narrower than 640px (small device screen width) and changes sidebar to top navbar
+            if (window.innerWidth <= 640) {
+                this.narrow = true;
+                this.maximized = false;
+                document.getElementById('main-container').classList.add('narrow');
+                document.getElementById('main-content').classList.remove('compensateMaximizedSidebar', 'compensateMinimizedSidebar');
+            }
+            else {
+                this.narrow = false;
+                this.expanded = false;
+                document.getElementById('main-container').classList.remove('narrow');
             }
         },
-        methods: {
-            // function to toggle value of "maximized" (toggle normal sidebar)
-            toggleSidebar() {
-                this.maximized = !this.maximized;
-            },
-            // function to toggle value of "expanded" (toggle narrow sidebar)
-            toggleNavbar() {
-                // if the navbar is going to be expanded then hide the main content so only the navbar is shown
-                // otherwise show the main content again
-                // save the user's current scroll location of the page to return the same content later
-                if (!this.expanded) {
-                    this.currentLocation = window.scrollY;
-                    document.getElementById('main-content').classList.add('hidden');
+        // to be called upon scroll up
+        scrollUp() {
+            const sidebar = document.getElementById('sidebar-wrapper');
+            const mainContent = document.getElementById('main-content');
+            
+            // if main content is shorter than sidebar then don't do anything
+            if (mainContent.offsetHeight <= sidebar.offsetHeight) {
+                return;
+            }
+
+            // release sidebar when it is fixed to the bottom, set it to position: absolute at its current position so the sidebar scrolls normally
+            if (sidebar.classList.contains('fixToBottom')) {
+                sidebar.classList.remove('fixToBottom');
+                sidebar.style.position = 'absolute';
+                sidebar.style.top = `${Math.abs(sidebar.getBoundingClientRect().top) - (sidebar.offsetHeight - window.innerHeight)}px`;
+                sidebar.style.left = '0';
+
+                return;
+            }
+            // stick sidebar to the top if the top of the sidebar touches the top of the window
+            else {
+                if (sidebar.getBoundingClientRect().top >= 0) {
+                    sidebar.classList.add('stickToTop');
+                    sidebar.style.position = '';
+                    sidebar.style.top = '';
+                    sidebar.style.left = '';
+                    mainContent.classList.remove('compensateMaximizedSidebar', 'compensateMinimizedSidebar');
+                }
+            }
+        },
+        // to be called upon scroll down
+        scrollDown() {
+            const sidebar = document.getElementById('sidebar-wrapper');
+            const mainContent = document.getElementById('main-content');
+            
+            // if main content is shorter than sidebar then don't do anything
+            if (mainContent.offsetHeight <= sidebar.offsetHeight) {
+                return;
+            }
+
+            // release sidebar when it sticks to the top, set it to position: absolute at its current position so the sidebar scrolls normally
+            if (sidebar.classList.contains('stickToTop')) {
+                sidebar.classList.remove('stickToTop');
+                sidebar.style.position = 'absolute';
+                sidebar.style.top = `${Math.abs(sidebar.getBoundingClientRect().top)}px`;
+                sidebar.style.left = '0';
+
+                if (document.getElementById('minimized')) {
+                    mainContent.classList.add('compensateMinimizedSidebar');
+                    mainContent.classList.remove('compensateMaximizedSidebar');
                 }
                 else {
-                    document.getElementById('main-content').classList.remove('hidden');
+                    mainContent.classList.add('compensateMaximizedSidebar');
+                    mainContent.classList.remove('compensateMinimizedSidebar');
                 }
 
-                this.expanded = !this.expanded;
-            },
-            // handle scroll logic
-            handleScroll() {
-                // don't do anything if sidebar is on narrow screens
-                if (this.narrow) {
-                    return;
-                }
-
-                // check if the scroll is upwards or downwards and handle it accordingly
-                if (window.scrollY < this.lastScrollY) {
-                    this.scrollUp();
-                }
-                else {
-                    this.scrollDown();
-                }
-
-                this.lastScrollY = window.scrollY;
-            },
-            // handle resize logic
-            handleResize() {
-                // check if window is narrower than 640px (small device screen width) and changes sidebar to top navbar
-                if (window.innerWidth <= 640) {
-                    this.narrow = true;
-                    this.maximized = false;
-                    document.getElementById('main-container').classList.add('narrow');
-                    document.getElementById('main-content').classList.remove('compensateMaximizedSidebar', 'compensateMinimizedSidebar');
-                }
-                else {
-                    this.narrow = false;
-                    this.expanded = false;
-                    document.getElementById('main-container').classList.remove('narrow');
-                }
-            },
-            // to be called upon scroll up
-            scrollUp() {
-                const sidebar = document.getElementById('sidebar-wrapper');
-                const mainContent = document.getElementById('main-content');
-                
-                // if main content is shorter than sidebar then don't do anything
-                if (mainContent.offsetHeight <= sidebar.offsetHeight) {
-                    return;
-                }
-
-                // release sidebar when it is fixed to the bottom, set it to position: absolute at its current position so the sidebar scrolls normally
-                if (sidebar.classList.contains('fixToBottom')) {
-                    sidebar.classList.remove('fixToBottom');
-                    sidebar.style.position = 'absolute';
-                    sidebar.style.top = `${Math.abs(sidebar.getBoundingClientRect().top) - (sidebar.offsetHeight - window.innerHeight)}px`;
-                    sidebar.style.left = '0';
-
-                    return;
-                }
-                // stick sidebar to the top if the top of the sidebar touches the top of the window
-                else {
-                    if (sidebar.getBoundingClientRect().top >= 0) {
-                        sidebar.classList.add('stickToTop');
-                        sidebar.style.position = '';
-                        sidebar.style.top = '';
-                        sidebar.style.left = '';
-                        mainContent.classList.remove('compensateMaximizedSidebar', 'compensateMinimizedSidebar');
-                    }
-                }
-            },
-            // to be called upon scroll down
-            scrollDown() {
-                const sidebar = document.getElementById('sidebar-wrapper');
-                const mainContent = document.getElementById('main-content');
-                
-                // if main content is shorter than sidebar then don't do anything
-                if (mainContent.offsetHeight <= sidebar.offsetHeight) {
-                    return;
-                }
-
-                // release sidebar when it sticks to the top, set it to position: absolute at its current position so the sidebar scrolls normally
-                if (sidebar.classList.contains('stickToTop')) {
-                    sidebar.classList.remove('stickToTop');
-                    sidebar.style.position = 'absolute';
-                    sidebar.style.top = `${Math.abs(sidebar.getBoundingClientRect().top)}px`;
-                    sidebar.style.left = '0';
+                return;
+            }
+            // fix sidebar at the bottom if the bottom of the sidebar touches the bottom of the window
+            else {
+                if ((Math.abs(sidebar.getBoundingClientRect().top) + window.innerHeight) >= sidebar.offsetHeight) {
+                    sidebar.classList.add('fixToBottom');
+                    sidebar.style.position = '';
+                    sidebar.style.top = '';
+                    sidebar.style.left = '';
 
                     if (document.getElementById('minimized')) {
                         mainContent.classList.add('compensateMinimizedSidebar');
@@ -283,93 +304,74 @@
                         mainContent.classList.add('compensateMaximizedSidebar');
                         mainContent.classList.remove('compensateMinimizedSidebar');
                     }
-
-                    return;
                 }
-                // fix sidebar at the bottom if the bottom of the sidebar touches the bottom of the window
-                else {
-                    if ((Math.abs(sidebar.getBoundingClientRect().top) + window.innerHeight) >= sidebar.offsetHeight) {
-                        sidebar.classList.add('fixToBottom');
-                        sidebar.style.position = '';
-                        sidebar.style.top = '';
-                        sidebar.style.left = '';
-
-                        if (document.getElementById('minimized')) {
-                            mainContent.classList.add('compensateMinimizedSidebar');
-                            mainContent.classList.remove('compensateMaximizedSidebar');
-                        }
-                        else {
-                            mainContent.classList.add('compensateMaximizedSidebar');
-                            mainContent.classList.remove('compensateMinimizedSidebar');
-                        }
-                    }
+            }
+        },
+        // to check for the current page and set the 'current' class for sidebar links corresponding to the current page
+        setCurrent() {
+            const links = document.querySelectorAll('.sidebar-link, .navbar-link');
+            for (var i = 0; i < links.length; i++) {
+                const link = links[i];
+                if (link.getAttribute('href').split('/')[1] == window.location.pathname.split('/')[1]) {
+                    link.classList.add('current');
                 }
-            },
-            // to check for the current page and set the 'current' class for sidebar links corresponding to the current page
-            setCurrent() {
-                const links = document.querySelectorAll('.sidebar-link, .navbar-link');
-                for (var i = 0; i < links.length; i++) {
-                    const link = links[i];
-                    if (link.getAttribute('href').split('/')[1] == window.location.pathname.split('/')[1]) {
-                        link.classList.add('current');
-                    }
-                    // set current for feed link if path is /
-                    else if (window.location.pathname == '/' && link.getAttribute('href').split('/')[1] == 'feed.html') {
-                        link.classList.add('current');
-                    }
-                    else {
-                        link.classList.remove('current');
-                    }
-                }
-            },
-            // to handle navbar behaviour on expand/collapse
-            setNavbar() {
-                if (!this.narrow) {
-                    return;
-                }
-
-                // if expand, scroll to the top where the expanded navbar is
-                // otherwise scroll back to the location where the user was before expanding
-                if (this.expanded) {
-                    window.scrollTo({
-                        top: 0,
-                        left: 0,
-                        behavior: 'instant'
-                    });
+                // set current for feed link if path is /
+                else if (window.location.pathname == '/' && link.getAttribute('href').split('/')[1] == 'feed.html') {
+                    link.classList.add('current');
                 }
                 else {
-                    window.scrollTo({
-                        top: this.currentLocation,
-                        left: 0,
-                        behavior: 'instant'
-                    });
+                    link.classList.remove('current');
                 }
-            },
-            // to set margins when sidebar is toggled to maximize/minimize
-            setMargin() {
-                if (this.narrow) {
-                    return;
-                }
+            }
+        },
+        // to handle navbar behaviour on expand/collapse
+        setNavbar() {
+            if (!this.narrow) {
+                return;
+            }
 
-                const sidebar = document.getElementById('sidebar-wrapper');
+            // if expand, scroll to the top where the expanded navbar is
+            // otherwise scroll back to the location where the user was before expanding
+            if (this.expanded) {
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'instant'
+                });
+            }
+            else {
+                window.scrollTo({
+                    top: this.currentLocation,
+                    left: 0,
+                    behavior: 'instant'
+                });
+            }
+        },
+        // to set margins when sidebar is toggled to maximize/minimize
+        setMargin() {
+            if (this.narrow) {
+                return;
+            }
 
-                if (sidebar.style.position != 'absolute' && !sidebar.classList.contains('fixToBottom')) {
-                    return;
-                }
+            const sidebar = document.getElementById('sidebar-wrapper');
 
-                const mainContent = document.getElementById('main-content');
+            if (sidebar.style.position != 'absolute' && !sidebar.classList.contains('fixToBottom')) {
+                return;
+            }
 
-                if (this.maximized) {
-                    mainContent.classList.remove('compensateMinimizedSidebar');
-                    mainContent.classList.add('compensateMaximizedSidebar');
-                }
-                else {
-                    mainContent.classList.remove ('compensateMaximizedSidebar');
-                    mainContent.classList.add('compensateMinimizedSidebar');
-                }
+            const mainContent = document.getElementById('main-content');
+
+            if (this.maximized) {
+                mainContent.classList.remove('compensateMinimizedSidebar');
+                mainContent.classList.add('compensateMaximizedSidebar');
+            }
+            else {
+                mainContent.classList.remove ('compensateMaximizedSidebar');
+                mainContent.classList.add('compensateMinimizedSidebar');
             }
         }
     }
+}
 </script>
 
 <style>
