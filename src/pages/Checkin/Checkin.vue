2f447ff5ae62b95f214e4f7d860809e52b25a32e
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2023-08-06 21:43:26 +0800

    update some styles

--- src/pages/Checkin/Checkin.vue
+++ src/pages/Checkin/Checkin.vue
@@ -92,16 +92,14 @@ h3 {
     display: flex;
     flex-direction: row;
     background-color: #0D283D;
-    padding: 50px 100px 50px 100px;
-    min-height: 40vh;
-    margin-left: -20px;
+    padding: 50px 80px;
+    min-height: 40dvh;
 }
 
 #dailyTasksDiv {
     background-color: #1C1B25;
-    padding: 50px 100px 50px 100px;
-    min-height: 60vh;
-    margin-left: -20px;
+    padding: 50px 80px;
+    min-height: 60dvh;
 }
 #scroll-view {
     overflow-x: scroll;
