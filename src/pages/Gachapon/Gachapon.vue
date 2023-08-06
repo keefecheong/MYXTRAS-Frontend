cd99f02a9f1fde8baf195ec56fcdfc5f4ef90180
Wee Kang Lee <jasonleeweekang@gmail.com>
2023-08-06 23:28:48 +0800

    gachapon responsiveness

--- src/pages/Gachapon/Gachapon.vue
+++ src/pages/Gachapon/Gachapon.vue
@@ -3,8 +3,9 @@
     <div id="main-container">
         <NavSidebar/>
         <div id="main-content" class="gachaponBody">
+            <button class="toggle-button" @click="toggleBetween">{{ viewingGacha ? 'View Inventory' : "Gachapon" }}</button>
             <Pet/>
-            <div class="left-content">
+            <div id="left-content" >
                 <div class="wallet-container">
                     <h2>
                         Your Wallet: <span class="material-symbols-outlined" style="display: inline;">account_balance_wallet</span>
@@ -48,7 +49,7 @@
                     </div>
                 </div>
             </div>
-            <div class="right-content">
+            <div id="right-content" >
                 <div class="machine">
                     <div id="redMachineHolder" class="gashaponHolder fitImg">
                         <img src="../../assets/red_machine_top.svg"/>
@@ -194,11 +195,11 @@ button:hover {
 .container {
     display: flex;
 }
-.left-content{
+#left-content{
     flex: 1;
     max-width: 100%;
 }
-.right-content {
+#right-content {
     flex: 2;
 }
 #buttons-container {
@@ -326,6 +327,46 @@ ul {
     min-width: 10rem;
     text-align: center;
 }
+.hide {
+    display: none;
+}
+.toggle-button {
+    display: none;
+}
+/* Mobile port */
+@media screen and (max-width: 768px) {
+body {
+    text-align: center !important;
+    font-size: 14px !important;
+}
+p {
+    font-size: 14px;
+}
+#white-container {
+    max-width: 100vw;
+}
+.toggle-button {
+    display: block;
+    border: 5px solid var(--primary);
+    background-color: var(--primary);
+}
+.toggle-button:hover {
+    background-color: transparent;
+}
+#left-content, #right-content {
+    width: 100%;
+}
+#main-content {
+    display: flex;
+    flex-direction: column;
+    padding: 0 10px;
+    min-height: 100vh;
+}
+#settings-options {
+    display: inline-block;
+    margin-left: 3vw
+}
+}
 </style>
 <script>
 
@@ -338,13 +379,50 @@ export default {
             inventoryPets: [],
             insufficientGems: false,
             deduction: null,
-            enabled: null
+            enabled: null,
+            viewingGacha: true,
         }
     },
     created(){
         this.initData();
     },
+    mounted() {
+        // Check if the current view is mobile or not
+        this.checkIsMobileView();
+        // Listen for window resize events to update the isMobileView property
+        window.addEventListener('resize', this.handleWindowResize);
+    },
+    beforeDestroy() {
+        // Remove the event listener to avoid memory leaks
+        window.removeEventListener('resize', this.handleWindowResize);
+    },
     methods: {
+        checkIsMobileView() {
+            const left_content = document.getElementById("left-content");
+            const right_content = document.getElementById("right-content");
+            // Use the window innerWidth to determine if it's mobile or not (you can adjust the breakpoint as needed)
+            if (window.innerWidth <= 767){
+                left_content.classList.add("hide");
+                right_content.classList.remove("hide")
+            }
+        },
+        handleWindowResize() {
+            // Update the isMobileView property whenever the window is resized
+            this.isMobileView = this.checkIsMobileView();
+        },
+        toggleBetween() {
+            const left_content = document.getElementById("left-content");
+            const right_content = document.getElementById("right-content");
+
+            this.viewingGacha = !this.viewingGacha; // Toggle
+            if (this.viewingGacha){
+                left_content.classList.add("hide");
+                right_content.classList.remove("hide")
+            } else {
+                left_content.classList.remove("hide");
+                right_content.classList.add("hide")
+            }
+        },
         getRarityStyle(rarity) {
             let color;
             switch (rarity) {
