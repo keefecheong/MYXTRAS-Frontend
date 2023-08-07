f61ab3ef5c05aa4508f965e1f1883efdc7f219c4
Yong Zi Ren <s10219524@connect.np.edu.sg>
2023-08-08 00:33:20 +0800

    semi-fixed errorpage

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,47 @@ import vue from '@vitejs/plugin-vue';
 const redirectIndexPlugin = {
     name: 'redirect-index',
     configureServer(server) {
+        const validRoutes = [];
         server.middlewares.use((req, res, next) => {
             // set root path to feed.html and set up redirection for feed router
             if (req.url === '/') {
                 req.url = '/feed.html';
+            }
+            else if (req.url === '/feed' || req.url === '/feed/' || req.url === '/feed.html') {
+                req.url = '/feed.html';
+            }
+            else if (req.url === '/chat' || req.url === '/chat/' || req.url === '/chat.html') {
+                req.url = '/chat.html';
+            }
+            else if (req.url === '/checkin' || req.url === '/checkin/' || req.url === '/checkin.html') {
+                req.url = '/checkin.html';
+            }
+            else if (req.url === '/events' || req.url === '/events/' || req.url === '/events.html') {
+                req.url = '/events.html';
+            }
+            else if (req.url === '/forum' || req.url === '/forum/' || req.url === '/forum.html') {
+                req.url = '/forum.html';
+            }
+            else if (req.url === '/forumGroup' || req.url === '/forumGroup/' || req.url === '/forumGroup.html') {
+                req.url = '/forumGroup.html';
+            }
+            else if (req.url === '/profilePage' || req.url === '/profilePage/' || req.url === '/profilePage.html') {
+                req.url = '/profilePage.html';
+            }
+            else if (req.url === 'profileManagement' || req.url === '/profileManagement/' || req.url === '/profileManagement.html') {
+                req.url = '/profileManagement.html';
+            }
+            else if (req.url === '/registration' || req.url === '/registration/' || req.url === '/registration.html') {
+                req.url = '/registration.html';
+            }
+            else if (req.url === '/setupProfile' || req.url === '/setupProfile/' || req.url === '/setupProfile.html') {
+                req.url = '/setupProfile.html';
+            }
+            else if (req.url === '/gachpon' || req.url === '/gachpon/' || req.url === '/gachpon.html') {
+                req.url = '/gachpon.html';
+            }
+            else if (req.url === '/login' || req.url === '/login/' || req.url === '/login.html') {
+                req.url = '/login.html';
             }
             // set up redirection for explore router
             else if (req.url === '/explore.html/blogs' || req.url === '/explore.html/threads') {
@@ -20,13 +57,17 @@ const redirectIndexPlugin = {
             }
             // set up redirection for admin pages
             else if (req.url === '/admin' || req.url === '/admin/') {
-                req.url = '/admin/home.html';
+                req.url = '/admin/reports.html';
             }
-            // redirect any 404 error to a custom error page (not working)
-            // else if (res.statusCode === 404) {
-            //     req.url = '/error.html';
-            // }
-            // add any other checks
+            else if (req.url === '/admin/reports' || req.url === '/admin/reports.html') {
+                req.url = '/admin/reports.html';
+            }
+            else if (req.url === '/admin/manageAccounts' || req.url === '/admin/manageAccounts.html') {
+                req.url = '/admin/manageAccounts.html';
+            }
+            else if (req.url === '/admin/events' || req.url === '/admin/events.html') {
+                req.url = '/admin/events.html';
+            }
             else if (req.url.endsWith('.html') || req.url.endsWith('.html/')) {
                 req.url = '/error.html';
             }
