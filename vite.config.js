a4e1eac3904ab889aa14697ac69fceb3262ba971
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2023-08-15 01:05:24 +0800

    change host to 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -53,7 +53,7 @@ export default defineConfig(({ command, mode }) => {
     return {
         plugins: [vue(), redirectPlugin],
         server: {
-            host: "127.0.0.1",
+            host: "0.0.0.0",
             port: env.VITE_PORT
         }
     }
